16e4b41a35f0e9b64f0857d1dba972ff6ed16528
grabeqx <grabowski.kontakt@gmail.com>
2017-11-16 12:22:49 +0100

    Create files structure

---
+++ app/vendor.js
@@ -0,0 +1,7 @@
+require('font-awesome/css/font-awesome.css');
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.min.css');
+require('../assets/style/main.scss');
+
+import angular from 'angular';
+import "regenerator-runtime/runtime";
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+var path = require('path');
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        index: path.join(__dirname,'/app/app.module.js'),
+        vendor: path.join(__dirname,'/app/vendor.js')
+    },
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: '[name].bundle.js',
+        publicPath: 'http://localhost/'
+    },
+    resolve: {
+        extensions : ['.js']
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin('[name].css'),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, '/app/index.html'),
+			inject: 'body',
+			filename: 'index.html'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            minChunks: Infinity
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default']
+        }),
+        new ngAnnotatePlugin({
+            add: true
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loaders: ['babel-loader']
+            },
+            {
+				test: /\.css$/,
+				use: ExtractTextWebpackPlugin.extract({
+                    fallback:'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.html$/,
+                use: 'raw-loader'
+            },
+            {
+				test: /\.scss$/,
+				use: ExtractTextWebpackPlugin.extract(['css-loader', 'sass-loader'])
+            },
+            { test:  /\.(jpg|png|svg)$/, use: [ "file-loader" ] },
+            { 
+                test:  /\.(jpg|png|svg)$/, 
+                include : path.join(__dirname, 'assets'),
+                use: [ "url-loader?limit=30000&name=images/[name].[ext]" ] 
+            },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+        ]
+    },
+    devServer: {
+        host: 'localhost',
+        port: 80,
+        historyApiFallback: true
+    }
+}
